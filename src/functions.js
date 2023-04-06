548fba1f493541b099c96704276ed37e2ba1bc18
Masteratom63 <95322440+Masteratom63@users.noreply.github.com>
2023-04-06 12:54:53 -0400

    Edit 1

--- src/functions.js
+++ src/functions.js
@@ -1,4 +1,4 @@
-import exp from "constants";
+import exp from 'constants';
 
 function hello(name) {
   return `hello ${name}`;
@@ -12,47 +12,38 @@ function SqFeet2Acre(SqFeet) {
   return Number(Acres.toFixed(2));
 }
 
-function Mowthelawn (width, length, cut) {
+function Mowthelawn(width, length, cut) {
   var minutes;
-  minutes = (width*length)/cut;
+  minutes = (width * length) / cut;
 
   return Number(minutes.toFixed(2));
 }
 
 function AirQualityIndex(AQI) {
-  if(AQI>=0 && AQI<=50) {
-    return ("Good");
-  }
-  else if(AQI>=51 && AQI<=100) {
-    return ("Moderate");
-  }
-  else if(AQI>=101 && AQI<=150) {
-    return ("Unhealthy for Sensitive Groups");
-  }
-  else if(AQI>=151 && AQI<=200) {
-    return ("Unhealthy");
-  }
-  else if(AQI>=201 && AQI<=300) {
-    return ("Very Unhealthy");
-  }
-  else {
-    return ("Hazardous");
+  if (AQI >= 0 && AQI <= 50) {
+    return 'Good';
+  } else if (AQI >= 51 && AQI <= 100) {
+    return 'Moderate';
+  } else if (AQI >= 101 && AQI <= 150) {
+    return 'Unhealthy for Sensitive Groups';
+  } else if (AQI >= 151 && AQI <= 200) {
+    return 'Unhealthy';
+  } else if (AQI >= 201 && AQI <= 300) {
+    return 'Very Unhealthy';
+  } else {
+    return 'Hazardous';
   }
 }
 
-function yee_ha (number) {
-
+function yee_ha(number) {
   if (number % 3 == 0 && number % 7 == 0) {
-    return ("Yee Ha")
-  }
-  else if (number % 3 == 0) {
-    return ("Yee")
-  }
-  else if (number % 7 == 0) {
-    return ("Ha")
-  }
-  else {
-    return {"Nada"}
+    return 'Yee Ha';
+  } else if (number % 3 == 0) {
+    return 'Yee';
+  } else if (number % 7 == 0) {
+    return 'Ha';
+  } else {
+    return 'Nada';
   }
 }
 
